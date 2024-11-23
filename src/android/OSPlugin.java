5050f113cabf69de0f9a24051b92dfe409b9fbe3
Ahmed Samir <asamir2211997@gmail.com>
2024-11-23 18:56:29 +0200

    add mapBox dependancies3

--- src/android/OSPlugin.java
+++ src/android/OSPlugin.java
@@ -6,6 +6,9 @@ import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 import android.content.Intent;
+import com.mapbox.geojson.Point;
+import com.mapbox.maps.CameraOptions;
+import com.mapbox.maps.MapView;
 
 public class OSPlugin extends CordovaPlugin {
 
@@ -26,6 +29,19 @@ public class OSPlugin extends CordovaPlugin {
             return true;
         }
         if ("showMap".equals(action)) {
+            // Create a map programmatically and set the initial camera
+            mapView = MapView(this)
+            mapView.mapboxMap.setCamera(
+                CameraOptions.Builder()
+                    .center(Point.fromLngLat(-98.0, 39.5))
+                    .pitch(0.0)
+                    .zoom(2.0)
+                    .bearing(0.0)
+                    .build()
+            )
+            // Add the map view to the activity (you can also add it to other views as a child)
+            setContentView(mapView)
+
             callbackContext.success("Map shown!");
             return true;
         }
